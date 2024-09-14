1752daefa12a09d7549dd0384d1e4d8845f2b220
ShmidtAA <Tyuiu.ShmidtAA@outlook.com>
2024-09-14 23:39:23 +0500

    Task4

--- Tyuiu.ShmidtAA.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.ShmidtAA.Sprint0.Task4.V0/Program.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ShmidtAA.Sprint0.Task4.V0
             Console.WriteLine(DataService.Additional(1, 5));
             Console.WriteLine(DataService.Subtraction(15,5));
             Console.WriteLine(DataService.Multiplication(10, 10));
-            Console.WriteLine(DataService.Division(5, 0));
+            Console.WriteLine(DataService.Division(5, 5));
             Console.ReadKey();
         }
     }
