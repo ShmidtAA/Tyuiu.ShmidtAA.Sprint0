826e199611bf8185fc7bbc68f5f56d65a0e57ec8
ShmidtAA <Tyuiu.ShmidtAA@outlook.com>
2024-09-14 19:36:55 +0500

    Task3

---
+++ Tyuiu.ShmidtAA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.ShmidtAA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int i, int b)
+        {
+            return i+ b;
+        }
+    }
+}
