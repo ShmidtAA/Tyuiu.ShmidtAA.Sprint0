569dea78c2179b04e5b7fa139b1a1f25c468899e
ShmidtAA <Tyuiu.ShmidtAA@outlook.com>
2024-09-14 18:07:28 +0500

    Task2

---
+++ Tyuiu.ShmidtAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.ShmidtAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ShmidtAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Игорь", res);
+        }
+    }
+}
