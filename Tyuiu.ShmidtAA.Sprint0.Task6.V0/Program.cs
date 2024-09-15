043b8f71aac5ad1f5badbc3ccd0b3a0a29db5d0a
ShmidtAA <Tyuiu.ShmidtAA@outlook.com>
2024-09-15 13:50:18 +0500

    Task6

--- Tyuiu.ShmidtAA.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.ShmidtAA.Sprint0.Task6.V0/Program.cs
@@ -7,11 +7,11 @@ namespace Tyuiu.ShmidtAA.Sprint0.Task6.V0
         {
             int [] arraynums= new int[] {1,2,3,4,5};
 
-            Console.WriteLine("Сумма элементов массива " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
 
-            Console.WriteLine("Разность элементов массива " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
 
-            Console.WriteLine("Произведение элементов массива " + DataService.MultiplicationArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
 
             Console.ReadKey();
         }
