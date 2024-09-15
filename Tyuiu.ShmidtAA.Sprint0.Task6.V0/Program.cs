900ea8d3a567c4a3309a7fb7b44f872f2132f066
ShmidtAA <Tyuiu.ShmidtAA@outlook.com>
2024-09-15 13:34:00 +0500

    Task6

--- Tyuiu.ShmidtAA.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.ShmidtAA.Sprint0.Task6.V0/Program.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.ShmidtAA.Sprint0.Task6.V0
     {
         static void Main(string[] args)
         {
-            var arraynums= new int[] {1,2,3,4,5};
+            int [] arraynums= new int[] {1,2,3,4,5};
 
             Console.WriteLine("Сумма элементов массива " + DataService.AdditionArray(arraynums));
 
